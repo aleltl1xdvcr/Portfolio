8ca3fc93041e8e10231b025addaa957a5f18dbe2
alexandrordnaxelalexandro@gmail.com <aleltl1xdv>
2025-06-12 23:53:48 -0600

    Adding languages

--- src/app/[language]/(main)/page.js
+++ src/app/[language]/(main)/page.js
@@ -3,6 +3,19 @@
 import  { getResults } from '../../(search)/(lib-search)/get-results'
 import Home from './Home'
 
+export async function generateStaticParams() {
+  return [
+    { language: 'es' },
+    { language: 'en' },
+    { language: 'fr' },
+    { language: 'pt' },
+    { language: 'ru' },
+    { language: 'ko' },
+    { language: 'de' },
+    { language: 'ja' },
+  ];
+}
+
 export default async function SearchPage({ params }) {
   const data = await getResults('ssr')
   return (
